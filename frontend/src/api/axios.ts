b97e70ff5f4b9751d210ab9e365111df7ed6052b
Bibhash-yadav <yadavbibhash304059@gmail.com>
2026-06-12 13:00:58 +0530

    live link

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://role-based-saas-billing-portal.onrender.com/",
 });
 
 API.interceptors.request.use((config) => {
